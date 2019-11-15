7e577f9fc89dfdc84225e6fc0c4d1f07c29e36a7
JannikBrink <jannik.brinkmann@it-service-brinkmann.de>
2019-11-15 23:45:51 +0100

    1. Step to automated model evaluation

--- modules/evaluation.py
+++ modules/evaluation.py
@@ -1,4 +1,16 @@
-def calculate_r_squared(res):
-    r_squared = 0
-    return r_squared
+from datetime import datetime
+from math import sqrt
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+
+def calculate_r_squared(y_actual, y_predicted):
+    r_squared = r2_score(y_actual, y_predicted)
+    print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), ": R_Squared = ", str(r_squared))
+
+
+def calculate_root_mean_squared_error(y_actual, y_predicted):
+    rmse = sqrt(mean_squared_error(y_actual, y_predicted))
+    print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), ": Root Mean Squared Error = ", str(rmse))
 
--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -1,13 +1,16 @@
+import warnings
 import numpy as np
 import pandas as pd
-import warnings
+
 warnings.filterwarnings("ignore")
 
 from datetime import datetime
 from sklearn import preprocessing
+from sklearn.preprocessing import OrdinalEncoder
+import geopandas as gpd
+from shapely.geometry import Point
 
 
-# Summarising function
 def load_and_preprocess_dataset():
     df = load_dataset()
     preprocessed_df = preprocess_dataset(df)
@@ -17,16 +20,27 @@ def load_and_preprocess_dataset():
 # Load the dataset containing airbnb listings in munich
 def load_dataset():
     df = pd.read_csv('data/listings_Munich_July.csv')
-
-    # Update user about the successful loading
     print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), ": Dataset loaded successfully.")
     return df
 
 
 # Preprocess the dataset containing airbnb listings in munich
 def preprocess_dataset(df):
-    # Select the relevant features
-    df = df.loc[:, df.columns.intersection(['accommodates',
+    df = feature_selection(df)
+    df = data_transformation(df)
+    df = instance_selection(df)
+    df = feature_extraction(df)
+    df = noise_identification(df)
+    df = data_cleaning(df)
+    df = data_normalization(df)
+
+
+    print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), ": Dataset preprocessed successfully.")
+    return df
+
+
+def feature_selection(df):
+    selected_features = df.loc[:, df.columns.intersection(['accommodates',
                                             'amenities',
                                             'bathrooms',
                                             'bedrooms',
@@ -40,42 +54,68 @@ def preprocess_dataset(df):
                                             'property_type',
                                             'room_type',
                                             'price'])]
+    return selected_features
 
-    # Preprocess the individual features
-    df = preprocess_price(df)
-    df = preprocess_bathrooms(df)
-    df = preprocess_bed_type(df)
-    df = preprocess_neighbourhood(df)
-    df = preprocess_property_type(df)
-    df = preprocess_room_type(df)
-    df = delete_outliers(df)
 
-    # Drop irrelevant features
-    df = df.drop(columns=['neighbourhood_cleansed', 'amenities'])
+def instance_selection(df):
+    # Delete commercial listings of property_type
+    property_types = df['property_type']
+    commercial_property_types = ['Boutique hotel', 'Hostel', 'Hotel', 'Aparthotel']
+    commercial_indices = []
+    for index, value in property_types.iteritems():
+        if value in commercial_property_types:
+            commercial_indices.append(index)
+    df = df.drop(commercial_indices)
 
-    # Update user about the successful pre-processing
-    print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), ": Dataset preprocessed successfully.")
+    # Delete special/unique listings (< 10 listings)
+    # special_property_types = ['Guesthouse', 'Villa', 'Hut', 'Tiny house', 'Casa particular (Cuba)', 'Earth house', 'Tipi', 'Bus', 'Castle', 'Cave', 'Farm stay', 'Resort', 'Cabin', 'Boat', 'Yurt']
+    # special_indices = []
+    # for index, value in property_types.iteritems():
+    #    if value in special_property_types:
+    #        special_indices.append(index)
+    # munich = munich.drop(special_indices)
+    # print(str(len(special_indices)), " special outliers deleted.")
     return df
 
 
-# Preprocess the price
-def preprocess_price(df):
-    # Cast 'price' to float & Delete leading dollar sign
+def noise_identification(df):
+    # Calculate mean and standard deviation
+    price = df['max_price']
+    price_mean = np.mean(price)
+    price_std = np.std(price)
+
+    # Detect noise and store their indices
+    noise_indices = []
+    for index, value in price.iteritems():
+        if value > (price_mean + (3 * price_std)):
+            noise_indices.append(index)
+
+    # Delete noise
+    df = df.drop(noise_indices)
+    return df
+
+
+def data_transformation(df):
+    # Convert 'price' to float & Delete leading dollar sign
     df['price'] = df['price'].replace('[\$,)]', '', regex=True).astype(float)
 
-    # Cast 'extra_people' to float & Delete leading dollar sign
+    # Convert 'extra_people' to float & Delete leading dollar sign
     df['extra_people'] = df['extra_people'].replace('[\$,)]', '', regex=True).astype(float)
+    return df
 
+
+def feature_extraction(df):
     # Calculate the maximum price for all listings
     for index, row in df.iterrows():
         if row.guests_included < row.accommodates:
             df.loc[index, 'max_price'] = row.price + (row.accommodates - row.guests_included) * row.extra_people
+            df.loc[index, 'max_people'] = row.accommodates - row.guests_included
         else:
             df.loc[index, 'max_price'] = row.price
+            df.loc[index, 'max_people'] = row.accommodates + row.guests_included
 
-    # Drop now obsolete features
-    df = df.drop(columns=['extra_people', 'guests_included', 'price'])
-    print("- Price preprocessed.")
+    # Calculate deviation from city center
+    df = geodata(df)
     return df
 
 
@@ -96,42 +136,36 @@ def preprocess_neighbourhood(df):
 
     # replace zero bedrooms with one bedroom
     df.loc[df.bedrooms == 0.0, 'bedrooms'] = 1.0
-
     return df
 
 
-# Preprocess the feature 'property_type'
-def preprocess_property_type(df):
+def data_normalization(df):
+    # encode property_type
     # Alternative 1: Label encoding of property type
-    label_encoder = preprocessing.LabelEncoder()
-    df['property_type'] = label_encoder.fit_transform(df['property_type'])
+    # label_encoder = preprocessing.LabelEncoder()
+    # df['property_type'] = label_encoder.fit_transform(df['property_type'])
 
     # Alternative 2: Ordinal encoding of property type
-    # ordinal_encoder = OrdinalEncoder()
-    # order = ordinal_encoder.fit_transform(munich[['property_type', 'price']].groupby('property_type').mean())
-    # property_types = munich['property_type'].unique()
-    # for index, property_type in enumerate(property_types):
-    #     munich[munich.property_type == property_type].property_type = order[index][0]
-    return df
+    ordinal_encoder = OrdinalEncoder()
+    order = ordinal_encoder.fit_transform(df[['property_type', 'max_price']].groupby('property_type').mean())
+    property_types = df['property_type'].unique()
+    for index, property_type in enumerate(property_types):
+        df.loc[df.property_type == property_type, 'property_type'] = order[index][0]
 
-
-# Preprocess the feature 'room_type'
-def preprocess_room_type(df):
+    # encode room_type
     # Alternative 1: Label encoding of room type
     label_encoder = preprocessing.LabelEncoder()
-    df['room_type'] = label_encoder.fit_transform(df['room_type'])
+    # df['room_type'] = label_encoder.fit_transform(df['room_type'])
 
     # Alternative 2: Ordinal encoding of property type
-    # ordinal_encoder = OrdinalEncoder()
-    # order = ordinal_encoder.fit_transform(munich[['room_type', 'price']].groupby('room_type').mean())
-    # room_types = munich['room_type'].unique()
-    # for index, room_type in enumerate(room_types):
-    #     munich[munich.room_type == room_type].room_type = order[index][0]
-    return df
+    ordinal_encoder = OrdinalEncoder()
+    order = ordinal_encoder.fit_transform(df[['room_type', 'max_price']].groupby('room_type').mean())
+    room_types = df['room_type'].unique()
+    for index, room_type in enumerate(room_types):
+        df.loc[df.room_type == room_type, 'room_type'] = order[index][0]
 
 
-# Preprocess the feature 'bed_type'
-def preprocess_bed_type(df):
+    # encode bathrooms
     # create replace map
     replace_map = {'bed_type': {'Couch': 1, 'Futon': 1, 'Pull-out Sofa': 1, 'Airbed': 2, 'Real Bed': 3}}
 
@@ -140,61 +174,29 @@ def preprocess_bed_type(df):
     return df
 
 
-# Preprocess the feature 'bathrooms'
-def preprocess_bathrooms(df):
+def data_cleaning(df):
     # replace blank entries with shared bathroom
     df.loc[df.bathrooms == None, 'bathrooms'] = 0.5
+    df.loc[df.bathrooms == 'NaN', 'bathrooms'] = 0.5
 
     # replace zero bathrooms with shared bathroom
     df.loc[df.bathrooms == 0.0, 'bathrooms'] = 0.5
     return df
 
 
-# Outlier detection and deletion via standard deviation
-def delete_outliers(df):
-    # Calculate mean and standard deviation
-    price = df['max_price']
-    price_mean = np.mean(price)
-    price_std = np.std(price)
+def geodata(df):
+    # Create point for every listing
+    geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])]
+    crs = {'init': 'epsg:4326'}
 
-    # Detect outliers and store their indices
-    outlier_indices = []
-    for index, value in price.iteritems():
-        if value > (price_mean + (3 * price_std)):
-            outlier_indices.append(index)
+    # create Geo-DataFrame with Points and Prices for each listing
+    geo_df = gpd.GeoDataFrame(df['max_price'], crs=crs, geometry=geometry)
 
-    # Delete outliers
-    df = df.drop(outlier_indices)
-    print("- ", str(len(outlier_indices)), " outliers deleted.")
+    # compute distance to Marienplatz as a proxy for the city centre
+    geo_df['distance_centre'] = geo_df.distance(Point(11.576006, 48.137079))
+    df['distance_centre'] = geo_df['distance_centre']
+
+    # discretise distance_centre
+    discretiser = preprocessing.KBinsDiscretizer(n_bins=10, encode='ordinal')
+    df['distance_centre'] = discretiser.fit_transform(df['distance_centre'].values.reshape(-1, 1))
     return df
-
-
-#def geodata():
-#    # read shapefile of Bavaria\n",
-#    munich_sh = 'Data//Geodata/gmd_ex.shp'
-#    map_df = gpd.read_file(munich_sh)
-#
-#    # Create point for every listing
-#    geometry = [Point(xy) for xy in zip(munich['longitude'], munich['latitude'])]
-#    munich = munich.drop(columns=['longitude', 'latitude'])
-#    crs = {
-#        'init': 'epsg:4326'}  # crs-data tells geopandas how the data relate to points in the real worlds (e.g. latitude and longitude)
-#    map_df = map_df.to_crs(epsg=4326)  # convert given shapefile to latitude and longitude-crs
-#
-#    # create Geo-DataFrame with Points and Prices for each listing
-#    geo_df = gpd.GeoDataFrame(munich['max_price'], crs=crs, geometry=geometry)
-#
-#    # create DataFrame for visuals
-#    geom = [Point(xy) for xy in zip(munich_visual['longitude'], munich_visual['latitude'])]
-#    geo_df_visual = gpd.GeoDataFrame(munich_visual['max_price'], crs=crs, geometry=geom)
-#
-#    # compute distance to Marienplatz as a proxy for the city centre
-#    geo_df['distance_centre'] = geo_df.distance(Point(11.576006, 48.137079))
-#
-#    # add feature to DataFrame and create a Geopandas-object for plotting
-#    munich['distance_centre'] = geo_df['distance_centre']
-#    point_marienplatz = gpd.GeoSeries(Point(11.576006, 48.137079), crs=crs)
-#
-# #discretise distance_centre
-# discretiser = preprocessing.KBinsDiscretizer(n_bins=5, encode='ordinal')
-# munich['distance_centre'] = discretiser.fit_transform(munich['distance_centre'].values.reshape(-1, 1))
